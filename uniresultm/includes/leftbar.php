2f8d433d905556022c496fd27f5aece8d156e5f0
subroto suvro <47747335+suvro1999@users.noreply.github.com>
2023-02-01 00:57:48 +0600

    New

---
+++ uniresultm/includes/leftbar.php
@@ -0,0 +1,78 @@
+<div class="left-sidebar bg-info color-black box-shadow ">
+                        <div class="sidebar-content ">
+                            <div class="user-info closed">
+                                <img src="http://siu.edu.bd/siu/img/logo.png" width = "80" height = "80" alt="siu logo" class="img-circle profile-img">
+                                <h6 class="title">SIU RMS</h6>
+                                <small class="info">Result management system </small>
+                            </div>
+                            <!-- /.user-info -->
+
+                            <div class="sidebar-nav">
+                                <ul class="side-nav color-gray">
+                                    <li class="nav-header">
+                                        <span class="">Main Category</span>
+                                    </li>
+                                    <li>
+                                        <a href="dashboard.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span> </a>
+                                     
+                                    </li>
+
+                                    <li class="nav-header">
+                                        <span class="">Appearance</span>
+                                    </li>
+                                    <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>Semesters</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="create-class.php"><i class="fa fa-bars"></i> <span>Create Semester</span></a></li>
+                                            <li><a href="manage-classes.php"><i class="fa fa fa-server"></i> <span>Manage Semester</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+  <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>Subjects</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="create-subject.php"><i class="fa fa-bars"></i> <span>Create Subject</span></a></li>
+                                            <li><a href="manage-subjects.php"><i class="fa fa fa-server"></i> <span>Manage Subjects</span></a></li>
+                                           <li><a href="add-subjectcombination.php"><i class="fa fa-newspaper-o"></i> <span>Add Subject Combination </span></a></li>
+                                           <a href="manage-subjectcombination.php"><i class="fa fa-newspaper-o"></i> <span>Manage Subject Combination </span></a></li>
+                                        </ul>
+                                    </li>
+   <li class="has-children">
+                                        <a href="#"><i class="fa fa-users"></i> <span>Dept. Head</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-dept.php"><i class="fa fa-bars"></i> <span>Add Dept. Head</span></a></li>
+                                            <li><a href="manage-dept.php"><i class="fa fa fa-server"></i> <span>Manage Dept. Head</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+   <li class="has-children">
+                                        <a href="#"><i class="fa fa-users"></i> <span>Teacher</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-teacher.php"><i class="fa fa-bars"></i> <span>Add Teacher</span></a></li>
+                                            <li><a href="manage-teacher.php"><i class="fa fa fa-server"></i> <span>Manage Teacher</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+   <li class="has-children">
+                                        <a href="#"><i class="fa fa-users"></i> <span>Students</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-students.php"><i class="fa fa-bars"></i> <span>Add Students</span></a></li>
+                                            <li><a href="manage-students.php"><i class="fa fa fa-server"></i> <span>Manage Students</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+<li class="has-children">
+                                        <a href="#"><i class="fa fa-info-circle"></i> <span>Result</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-result.php"><i class="fa fa-bars"></i> <span>Add Result</span></a></li>
+                                            <li><a href="manage-results.php"><i class="fa fa fa-server"></i> <span>Manage Result</span></a></li>
+                                           
+                                        </ul>
+                                        <li><a href="change-password.php"><i class="fa fa fa-server"></i> <span> Admin Change Password</span></a></li>
+                                           
+                                    </li>
+                            </div>
+                            <!-- /.sidebar-nav -->
+                        </div>
+                        <!-- /.sidebar-content -->
+                    </div>
---
+++ uniresultm/includes/leftbar1.php
@@ -0,0 +1,46 @@
+<div class="left-sidebar bg-info color-black box-shadow ">
+                        <div class="sidebar-content ">
+                            <div class="user-info closed">
+                                <img src="http://siu.edu.bd/siu/img/logo.png" width = "80" height = "80" alt="siu logo" class="img-circle profile-img">
+                                <h6 class="title">SIU RMS</h6>
+                                <small class="info">Result management system </small>
+                            </div>
+                            <!-- /.user-info -->
+
+                            <div class="sidebar-nav">
+                                <ul class="side-nav color-gray">
+                                    <li class="nav-header">
+                                        <span class="">Main Category</span>
+                                    </li>
+                                    <li>
+                                        <a href="dashboard.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span> </a>
+                                     
+                                    </li>
+
+                                    <li class="nav-header">
+                                        <span class="">Appearance</span>
+                                    </li>
+                                    
+  <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>Add Result</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-st-result.php"><i class="fa fa-bars"></i> <span>Added Result</span></a></li>
+                                            <li><a href=""><i class="fa fa fa-server"></i> <span>View Result</span></a></li>
+                                            <li><a href=""><i class="fa fa-bars"></i> <span>Update Result</span></a></li>
+                                           <!-- <li><a href="add-subjectcombination.php"><i class="fa fa-newspaper-o"></i> <span>Add Subject Combination </span></a></li>
+                                           <a href="manage-subjectcombination.php"><i class="fa fa-newspaper-o"></i> <span>Manage Subject Combination </span></a></li> -->
+                                        </ul>
+                                    </li>
+   <li class="has-children">
+                                        <a href="#"><i class="fa fa-users"></i> <span>Students</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-students.php"><i class="fa fa-bars"></i> <span>View Student</span></a></li>
+                                            <li><a href="manage-students.php"><i class="fa fa fa-server"></i> <span>View Semester</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+                            </div>
+                            <!-- /.sidebar-nav -->
+                        </div>
+                        <!-- /.sidebar-content -->
+                    </div>
